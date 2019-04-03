e8b137dcfa2f8ffe90a5414949433e350ab5ab98
anderson3661 <martin.anderson3661@gmail.com>
2019-04-03 13:41:09 +0100

    Cache updates

--- src/index.js
+++ src/index.js
@@ -5,4 +5,9 @@ import App from './App';
 
 import './index.scss';
 
+// When deploying in production, in Chrome the app doesn't display the latest updates unless you unregister the service worker below.
+// Also in firebase.json added "headers" under "hosting" to sepcify no cache
+import { unregister } from './registerServiceWorker';
+unregister() ;
+
 ReactDOM.render(<App />, document.getElementById('root'));
